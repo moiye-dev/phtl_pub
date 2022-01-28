2458c03a6ee9dca4ce1cfa23e03108ed8b281aac
Saurabh Mehra <saurabh.mehra@soft-project.de>
2022-01-28 12:07:06 +0800

    Helpers

--- lib/grid_widget.dart
+++ lib/grid_widget.dart
@@ -5,3 +5,4 @@ export 'package:phtl_pub/src/permissions_flushbar.dart';
 export 'package:phtl_pub/src/commons.dart';
 export 'package:phtl_pub/src/custom_alert_dialog.dart';
 export 'package:phtl_pub/src/bordered_label.dart';
+export 'package:phtl_pub/src/headless/helpers.dart';
---
+++ lib/src/headless/helpers.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: avoid_print
+
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Helpers {
+  static final Helpers _instance = Helpers._internal();
+
+  factory Helpers() {
+    return _instance;
+  }
+
+  Future<void> initPlatformState() async {
+    await dotenv.load();
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+    String? appId = dotenv.env['AppId'];
+    print("appId=$appId");
+    await OneSignal.shared.setAppId(appId ?? "");
+
+    //iOS requires users consent for notifications
+    // Android devices are subscribed to notifications automatically when your app is installed,
+    OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      OneSignal.shared.getDeviceState().then((deviceState) {
+        if (deviceState != null) {
+          //@TODO
+          // print("PlayerId=" + deviceState.userId!);
+          // print("Push Disabled=${deviceState.pushDisabled}");
+        }
+      });
+    });
+  }
+
+  void setExternalUser(String externalUserId) {
+    OneSignal.shared
+        .setExternalUserId(externalUserId)
+        .then((results) {})
+        .catchError((error) {});
+  }
+
+  Helpers._internal();
+}
